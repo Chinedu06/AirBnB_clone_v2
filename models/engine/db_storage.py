a19a32978012914a8cbe62198c65c91cfea5ae00
chinedu06 <godswillchinedu644@gmail.com>
2024-07-31 13:21:26 +0000

    first commit

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -11,8 +11,23 @@ from models.base_model import Base
 class DBStorage:
     """DBStorage class manages storage of hbnb models in MySQL database"""
 
-    # existing code
+    __engine = None
+    __session = None
+
+    # existing code ...
 
     def close(self):
-        """Call remove() methodontheprivatesessionattribute (self.__session)"""
+        """
+        Call remove() method on the private
+        session attribute (self.__session)
+        """
         self.__session.remove()
+
+    def reload(self):
+        """Create all tables in the database and start a new session"""
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(
+            bind=self.__engine,
+            expire_on_commit=False
+        )
+        self.__session = scoped_session(session_factory)
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,7 +11,10 @@ from models.user import User
 class FileStorage:
     """FileStorage class manages storage of hbnb models in JSON format"""
 
-    # existing code
+    __file_path = "file.json"
+    __objects = {}
+
+    # existing code ...
 
     def close(self):
         """Call reload() method for deserializing the JSON file to objects"""
